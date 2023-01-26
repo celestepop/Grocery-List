355e5dcf80d4021649d7bc5b801648ed329d46c0
celestepop <celeste3popoca@gmail.com>
2023-01-25 19:12:15 -0700

    cleaned up

--- src/Item.java
+++ src/Item.java
@@ -1,16 +1,11 @@
 public class Item {
     String name;
     float price;
-    // int quantity;
 
     public Item(String itemName, float itemPrice) {
         name = itemName;
         price = itemPrice;
-        // quantity = 0;
     }
-    // public static void main(String[] args) throws Exception {
-        
-    // }
 
     public String getItemName() {
         return name;
